a7c6663e8cb92031a331ab4edab27bb5a5b99049
Manas Salunke <123manassalunke@gmail.com>
2024-10-10 18:42:15 +0530

    feat: add auth route folders

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,5 @@
+const SignInPage = () => {
+  return <div>signin</div>;
+};
+
+export default SignInPage;
---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return <Button variant="destructive">This is an authenticated route</Button>;
+}
