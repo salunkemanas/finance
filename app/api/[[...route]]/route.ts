3fd876324825a2a986e4cdefba28cb72e6d5c890
Manas Salunke <123manassalunke@gmail.com>
2024-10-11 18:33:34 +0530

    feat: complete hono routing

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,13 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-
+import authors from "./authors";
+import books from "./books";
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
-});
+app.route("/authors", authors);
+app.route("/books", books);
 
 export const GET = handle(app);
 export const POST = handle(app);
